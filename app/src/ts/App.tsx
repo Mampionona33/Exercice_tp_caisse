5622edad92d64d7d1fc520e307df95285f0044c0
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-08-31 07:58:50 +0000

    using useContext on the productfamilly list

--- app/src/ts/App.tsx
+++ app/src/ts/App.tsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import AddToCart from "./components/AddToCart";
 import Cart from "./components/Cart";
 import ProductFamilyGroupe from "./components/ProductFamilyGroupe";
 import ProductList from "./components/ProductList";
 
+export interface IProductFamilly {
+  idProdFamilly: number;
+  name: string;
+}
+
+export interface IProduct {
+  ref: number;
+  familly: IProductFamilly;
+  price: number;
+  brand: string;
+}
+
+// Création des contextes
+export const ProductFamillyContext = createContext<IProductFamilly[]>([]); 
+export const ProductListContext = createContext<IProduct[]>([]);
+
 const App: React.FC = () => {
+  const productFamilies: IProductFamilly[] = [
+    { idProdFamilly: 0, name: "biscuit" },
+    { idProdFamilly: 1, name: "savon" },
+    { idProdFamilly: 2, name: "déodorant" },
+    { idProdFamilly: 3, name: "huile" },
+  ];
+
+  const productList: IProduct[] = [
+    {
+      ref: 0,
+      familly: {
+        idProdFamilly: 0,
+        name: "biscuit",
+      },
+      brand: "Bolo",
+      price: 200,
+    },
+  ];
+
   return (
-    <>
-      <div className="container-lg d-flex gap-3">
-        <div className="col-8">
-          <Cart />
-        </div>
-        <div className="col-4 d-flex flex-column">
-          <h2>Net à payer:</h2>
-          <AddToCart/>
+    <div className="container-lg d-flex gap-3">
+      <div className="col-4 d-flex flex-column">
+        <h2>Net à payer:</h2>
+        <AddToCart />
+        <ProductFamillyContext.Provider value={productFamilies}>
           <ProductFamilyGroupe />
+        </ProductFamillyContext.Provider>
+        <ProductListContext.Provider value={productList}>
           <ProductList />
-        </div>
+        </ProductListContext.Provider>
       </div>
-    </>
+      <div className="col-8">
+        <Cart />
+      </div>
+    </div>
   );
 };
+
 export default App;
--- app/src/ts/components/ProductFamilyGroupe.tsx
+++ app/src/ts/components/ProductFamilyGroupe.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductFamillyContext, IProductFamilly } from "../App";
 
-const ProductFamilyGroupe:React.FC =()=>{
+const ProductFamilyGroupe: React.FC = () => {
+  const productFamilies: IProductFamilly[] = useContext(ProductFamillyContext);
 
-    return(
-        <div>
-            <div>list famille de produits</div>
-        </div>
-    )
+  return (
+    <div>
+      <div>Liste des familles de produits :</div>
+      <div className="d-flex gap-2">
+        {productFamilies.map((family) => (
+          <button
+            key={family.idProdFamilly}
+            className="btn btn-outline-primary"
+          >
+            {family.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProductFamilyGroupe;
