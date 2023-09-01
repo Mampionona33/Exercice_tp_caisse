ed45843a89dce399e26f0b9edbaf0b5f6c345c9d
Mampionona33 <ramamps33@gmail.com>
2023-09-01 19:45:27 +0300

    install material icons

--- app/src/ts/components/AddToCart.tsx
+++ app/src/ts/components/AddToCart.tsx
@@ -7,12 +7,28 @@ const AddToCart: React.FC = () => {
       <Form>
         <Form.Group>
           <Form.Label>Quantité</Form.Label>
-          <Form.Control type="number" min={0}/>
+          <Form.Control type="number" min={0} />
         </Form.Group>
       </Form>
       <div className="d-flex gap-2">
-        <Button variant="danger">Effacer</Button>{" "}
-        <Button variant="primary">Ajouter au panier</Button>{" "}
+        <Button
+          variant="danger"
+          className="d-flex justify-content-center align-items-center"
+        >
+          <span className="material-icons-outlined ">clear</span>
+        </Button>
+        <Button
+          variant="primary"
+          className="d-flex justify-content-center align-items-center"
+        >
+          <span className="material-icons-outlined">add_shopping_cart</span>
+        </Button>
+        <Button
+          variant="primary"
+          className="d-flex justify-content-center align-items-center"
+        >
+          <span className="material-icons-outlined">euro</span>
+        </Button>
       </div>
     </div>
   );
