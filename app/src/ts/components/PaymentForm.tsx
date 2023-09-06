d3ce92f7020e06b72deb8e1c4e7e5234ae2241b8
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-06 11:10:30 +0000

    Update PaymentForm.tsx: Vita ahh

--- app/src/ts/components/PaymentForm.tsx
+++ app/src/ts/components/PaymentForm.tsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
+import { useGlobalData } from "../context/GlobalDataContext";
 
-const PaymentForm = () =>{
-    return(
-        <>
-            <Form>
-                <Form.Group controlId="form.hasToPay">
-                    <Form.Label>Net à payer</Form.Label>
-                    <Form.Control type="number" min={0} required />
-                </Form.Group>
-            </Form>
-        </>
-    )
-}
+const PaymentForm: React.FC = () => {
+  const [argentRecu, setArgentRecu] = useState<number | string>("");
+  const { prodInCart } = useGlobalData();
+
+  const calculateTotalPrice = () => {
+    if (prodInCart) {
+      return prodInCart.reduce((acc, curr) => acc + curr.amount!, 0);
+    }
+    return 0;
+  };
+
+  const calculateMoneyToReturn = () => {
+    const argentRecuValue = parseFloat(argentRecu.toString());
+    if (isNaN(argentRecuValue)) {
+      return "";
+    }
+
+    console.log(calculateTotalPrice());
+    console.log(argentRecuValue);
+
+    const argentARendre = argentRecuValue - calculateTotalPrice();
+    // Ajoutez une vérification pour éviter les montants négatifs
+    const montantRendu = argentARendre >= 0 ? argentARendre.toFixed(2) : 0;
+
+    return montantRendu;
+  };
+
+  return (
+    <>
+      <Form className="d-flex gap-3 flex-column">
+        <Form.Group controlId="form.argentRecu">
+          <Form.Label className="text-capitalize">Argent reçu</Form.Label>
+          <Form.Control
+            type="number"
+            min={0}
+            value={argentRecu}
+            onChange={(e) => setArgentRecu(e.target.value)}
+            placeholder="Argent reçu en euros"
+            required
+          />
+        </Form.Group>
+        <div className="bg-success p-2 text-white" >
+          À rendre : <span>{calculateMoneyToReturn()}</span> €
+        </div>
+      </Form>
+    </>
+  );
+};
 
 export default PaymentForm;
