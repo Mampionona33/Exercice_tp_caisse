c8d839f8671390d9dc34ab570b3bea8c29bd15a1
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-06 12:08:50 +0000

    Update CartTable.tsx: Vitaaaa

--- app/src/ts/components/CartTable.tsx
+++ app/src/ts/components/CartTable.tsx
@@ -2,38 +2,51 @@ import React from "react";
 import { useGlobalData } from "../context/GlobalDataContext";
 
 const CartTable: React.FC = () => {
-  const {prodInCart} = useGlobalData();
+  const { prodInCart, setProdInCart } = useGlobalData();
 
-  const createBody =()=>{
-    return prodInCart?.map((pro)=>{
-      return(
-        <tr>
+  const handleDeleteProd = (ref: number) => {
+    // Supprimer le produit avec la référence (ref) donnée du panier
+    if (prodInCart) {
+      const updatedProdInCart = prodInCart.filter((prod) => prod.ref !== ref);
+      setProdInCart(updatedProdInCart);
+    }
+  };
+
+  const createBody = () => {
+    return prodInCart?.map((pro) => {
+      return (
+        <tr key={pro.ref}>
           <th>{pro.ref}</th>
           <th>{pro.designation}</th>
           <th>{pro.price}</th>
           <th>{pro.quantity}</th>
           <th>{pro.amount}</th>
-          <th><button className="btn btn-danger">supprimer</button></th>
+          <th>
+            <button
+              onClick={() => handleDeleteProd(pro.ref!)}
+              className="btn btn-danger"
+            >
+              Supprimer
+            </button>
+          </th>
         </tr>
-      )
-    })
-  }
+      );
+    });
+  };
 
   return (
-    <table className="table ">
+    <table className="table">
       <thead className="table-dark">
         <tr>
-          <th className="text-capitalize align-top">article</th>
-          <th className="text-capitalize align-top">designation</th>
-          <th className="text-capitalize align-top">prix HT</th>
-          <th className="text-capitalize align-top">quantité</th>
-          <th className="text-capitalize align-top">montant</th>
+          <th className="text-capitalize align-top">Article</th>
+          <th className="text-capitalize align-top">Désignation</th>
+          <th className="text-capitalize align-top">Prix HT</th>
+          <th className="text-capitalize align-top">Quantité</th>
+          <th className="text-capitalize align-top">Montant</th>
           <th className="text-capitalize align-top">Action</th>
         </tr>
       </thead>
-      <tbody>
-        {createBody()}
-      </tbody>
+      <tbody>{createBody()}</tbody>
     </table>
   );
 };
